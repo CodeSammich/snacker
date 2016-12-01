87207d8912ac824d47db4f6566822cc38db7688e
Samuel Zhang <samuelzhang3@gmail.com>
2016-12-01 18:46:36 -0500

    add user function in mongo init connect

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 const http = require('http');
+const mongodb = require('mongodb');
 
-http.createServer((req, res) => {
+var MongoClient = mongodb.MongoClient;
+var mongoUrl = 'mongodb://localhost:1337/accountsDatabase';
+
+function addUser(userName) {
+    /*
+      Add a new user to 'usersCollection' collection
+     */
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+            // connected to database
+            console.log('Connection established to', url);
+
+            // do something to database
+            var userCollection = db.collection('usersCollection');
+
+            // Close connection
+            db.close();
+        }
+    });
+}
+
+http.createServer((req, res) => { // arrow function, like lambda expression
 
     // 1. Tell browser everything is okay
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -9,4 +33,4 @@ http.createServer((req, res) => {
     res.end('Hello, World!\n');
 }).listen(1337);
 
-console.log("Server is running at http://127.0.0.1337");
+console.log("Server is running at http://localhost:1337");
